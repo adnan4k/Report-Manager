c6957d3f2f8622dc5a6d847c3abfd750a0ef97d4
adnan4k <fayomuhe5@gmail.com>
2024-09-21 07:03:46 +0300

    dashbaord core point completed

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -3,18 +3,41 @@
 namespace App\Http\Livewire;
 
 use App\Models\Business;
+use App\Models\Report;
+use App\Models\User;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
-    public $totalMoney;
-    public function mount(){
-       
-      $this->totalMoney = Business::select('price')->get();
-    //   dd($this->totalMoney);
-    }
-    public function render()
-    {
-        return view('livewire.dashboard');
-    }
+  public $totalMoney;
+  public $totalUsers;
+  public $reported;
+  public $unreported;
+
+  public function mount()
+  {
+
+    $this->totalMoney = Business::sum('price');
+    $this->totalUsers = User::count();
+
+
+    $this->reported = Report::where(function ($query) {
+      $query->where('statement_status', 1)
+        ->where('tax_status', 1)
+        ->where('payroll_status', 1);
+    })->count();
+
+
+    $this->unreported = Report::where(function ($query) {
+      $query->where('statement_status', 0)
+        ->where('tax_status', 0)
+        ->where('payroll_status', 0);
+    })->count();
+
+    // dd($this->reported);
+  }
+  public function render()
+  {
+    return view('livewire.dashboard');
+  }
 }
